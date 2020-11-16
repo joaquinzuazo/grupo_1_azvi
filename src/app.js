7989bbf386e1eb94bd9e2b657c046297d57483da
Facundo Aquino <facun90@live.com>
2020-11-16 11:02:24 -0300

    agregando rutas y estilos generales

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-const express = require ('express')
- 
-const app = express()
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
 
 
+// Rutas 
 
-app.listen(3000)
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/productDetail', (req, res) => {
+	res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/productCart', (req, res) => {
+	res.sendFile(__dirname + '/views/productCart.html');
+});
+app.get('/login', (req, res) => {
+	res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/register', (req, res) => {
+	res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(3000);
