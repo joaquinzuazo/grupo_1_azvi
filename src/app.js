8a582a93dcdc74eb6d8ab240f4ac0dbbb388239a
Facundo Aquino <facun90@live.com>
2020-11-19 10:36:55 -0300

    index y estilos

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-
-// Rutas 
+// Rutas
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/index.html');
-});
+	res.sendFile(__dirname + '/views/index.html')
+})
 
 app.get('/productDetail', (req, res) => {
-	res.sendFile(__dirname + '/views/productDetail.html');
-});
+	res.sendFile(__dirname + '/views/productDetail.html')
+})
 
 app.get('/productCart', (req, res) => {
-	res.sendFile(__dirname + '/views/productCart.html');
-});
+	res.sendFile(__dirname + '/views/productCart.html')
+})
 app.get('/login', (req, res) => {
-	res.sendFile(__dirname + '/views/login.html');
-});
+	res.sendFile(__dirname + '/views/login.html')
+})
 app.get('/register', (req, res) => {
-	res.sendFile(__dirname + '/views/register.html');
-});
+	res.sendFile(__dirname + '/views/register.html')
+})
 
-app.listen(3000);
+app.listen(3000)
