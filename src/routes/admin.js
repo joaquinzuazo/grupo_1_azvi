1b179d820938ee2117a7832ba58bdfba07702ca9
Facundo Aquino <facun90@live.com>
2020-12-15 15:17:12 -0300

    add partials, head/header/footer

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,11 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/create', (req, res) => {
-	res.render('adminAdd')
+	res.render('adminAdd', { style: 'admin' })
 })
 
 router.get('/update', (req, res) => {
-	res.render('adminUpdate')
+	res.render('adminUpdate', { style: 'admin' })
 })
 
 module.exports = router
--- src/routes/productDetail.js
+++ src/routes/productDetail.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('productDetail', { title: 'AZVI' });
-});
+router.get('/', function (req, res, next) {
+	res.render('productDetail', { title: 'AZVI', style: 'productos' })
+})
 
-module.exports = router;
+module.exports = router
