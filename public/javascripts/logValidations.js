4c8ceba6be8e30e9a08fdd688676da45a7269de3
Facundo Aquino <facun90@live.com>
2021-03-10 15:58:11 -0300

    improving login to see user password

--- public/javascripts/logValidations.js
+++ public/javascripts/logValidations.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function () {
 	let formulario = document.querySelector('form')
+	let campoPass = document.querySelector('#pass')
 	formulario.addEventListener('submit', function (e) {
 		e.preventDefault()
 		let errores = []
 		let ulErrores = document.querySelector('#article-container-form-regis-error')
 		let campoEmail = document.querySelector('#email')
-		let campoPass = document.querySelector('#pass')
 		let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
 		if (campoPass.value == '') {
@@ -20,8 +20,17 @@ window.addEventListener('load', function () {
 			for (error of errores) {
 				ulErrores.innerHTML += `<p style="font-size:14px"><i class="fas fa-exclamation-triangle" style="color: rgb(199, 199, 26); font-size:12px;"></i>  ${error}</p>`
 			}
-            
-            `<img src=${informacion.data[i].images.url}>`
+
+			;`<img src=${informacion.data[i].images.url}>`
 		} else [this.submit()]
+	})
+
+	const formIconPassword = document.querySelector('.form-icon-password')
+
+	formIconPassword.addEventListener('click', (e) => {
+		e.target.classList.toggle('form-icon-efffect')
+		campoPass.getAttribute('type') == 'Password'
+			? (campoPass.setAttribute('type', 'text'), e.target.classList.replace('fa-eye-slash', 'fa-eye'))
+			: (campoPass.setAttribute('type', 'Password'), e.target.classList.replace('fa-eye', 'fa-eye-slash'))
 	})
 })
