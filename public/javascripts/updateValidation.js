8581ebc199614ce7f3c5fdad5049407f815f8b06
tomasVergara14 <tomasvergara272@gmail.com>
2021-03-12 13:19:34 -0300

    Validacion update

---
+++ public/javascripts/updateValidation.js
@@ -0,0 +1,52 @@
+
+const qs = text => document.querySelector(text);
+const qsa = text => document.querySelectorAll(text)
+
+window.addEventListener("load", ()=>{
+
+    let form = qs(".mainAdmin__form")
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        
+        console.log("hola")
+
+        let name = qs("#name")
+        let lastname =  qs("#lastname")
+        let cellphone = qs("#cellphone")
+        let email = qs("#email")
+        let photo = qs("#photo")
+        let errors = qs(".errors")
+
+        let errores = []
+        if (name.value == ""){
+            errores.push ("El campo de Nombre tiene que estar completo") 
+        } else if (name.value.length < 5){
+            errores.push ("El campo de Nombre debe tener al menos 5 caracteres") ;
+        }
+
+        if (lastname.value == ""){
+            errores.push ("El campo de Apellido tiene que estar completo") ;
+        } else if (lastname.value.length < 5){
+            errores.push ("El campo de Apellido debe tener al menos 5 caracteres") ;
+        }
+
+        if (cellphone.value == ''){
+            errores.push ('El campo de celular no puede estar vacio ') 
+        } else if ( isNaN(cellphone.value)){
+            errores.push('El campo de celular acepta solo numeros')
+        }
+
+        let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+        if (!emailRegex.test(email.value)){
+            errores.push ( "Ingrese un email con formato valido.");
+        }
+
+        if (errores.length > 0) {
+            
+            errores.forEach((error)=>{
+                errors.innerHTML +=  "<li>" + error + "</li>"
+                errors.style.color = "red"
+            })
+        } else [form.submit()]
+    })
+})
