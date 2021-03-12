b19baf9dfe120f0fe7b6c4c9751f94434e3f05b4
Facundo Aquino <facun90@live.com>
2021-03-11 23:58:50 -0300

    show avatar on user data and  input file styles

--- public/javascripts/userData.js
+++ public/javascripts/userData.js
@@ -1,17 +1,34 @@
 const inputFile = document.getElementById('avatar_file')
 const errorContainer = document.querySelector('.validationMessage')
 const formAvatar = document.querySelector('.mainAdmin_personalImage_form')
-
+const avatarImg = document.querySelector('.mainAdmin__avatarImg')
+const buttona = document.querySelector('.mainAdmin__inputfileContainer--button')
+buttona.addEventListener('click',e=> inputFile.click())
 const checkSize = (element) => {
 	errorContainer.innerHTML = ''
-	if (element.files[0].size > 2097152) {
+	if (element.files[0]?.size > 2097152) {
 		errorContainer.innerHTML = `<i class="validationMessage--i fas fa-exclamation-triangle"></i>El archivo no puede superar los 2 Mb`
 		return true
 	}
 }
 
+const ORIGINAL_AVATAR_SRC = avatarImg.getAttribute('src')
+
 inputFile.addEventListener('change', (e) => {
-	checkSize(e.target)
+	const fileIsBig = checkSize(e.target)
+	if (!fileIsBig) {
+		const reader = new FileReader()
+		// console.log(e.target.files);
+		if (e.target.files[0]) {
+			reader.readAsDataURL(e.target.files[0])
+
+			reader.addEventListener('load', () => {
+				avatarImg.src = reader.result
+			})
+		} else {
+			avatarImg.src = ORIGINAL_AVATAR_SRC
+		}
+	}
 })
 
 formAvatar.addEventListener('submit', (e) => {
