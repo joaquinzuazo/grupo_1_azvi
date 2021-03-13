3d4aba37ec0723d6d77fa1b5c692b1a1b4c7c53d
Facundo Aquino <facun90@live.com>
2021-03-13 00:17:14 -0300

    user location repair

--- public/javascripts/setLocation.js
+++ public/javascripts/setLocation.js
@@ -7,14 +7,13 @@ const getLocation = async ({ latitude, longitude }) => {
 	const { ubicacion } = await response.json()
 	const { municipio, provincia } = ubicacion
 	const session = { provincia: provincia.nombre, municipio: municipio.nombre }
-	console.log('seteado')
 	sessionStorage.setItem('location', JSON.stringify(session))
 
-	locationText.innerHTML = `<i class="fas fa-map-marker-alt"> </i><p  style="display: inline; margin-left: 3px;">${municipio.nombre}</p>`
+	locationText.innerHTML = `<i class="fas fa-map-marker-alt"> </i><p  style="display: inline; margin-left: 3px;">${municipio.nombre||provincia.nombre}</p>`
 }
 
 if (!userLocation) {
 	navigator.geolocation.getCurrentPosition(({ coords }) => getLocation(coords))
 } else {
-	locationText.innerHTML = `<i class="fas fa-map-marker-alt"> </i><p  style="display: inline; margin-left: 3px;">${userLocation.municipio}</p>`
+	locationText.innerHTML = `<i class="fas fa-map-marker-alt"> </i><p  style="display: inline; margin-left: 3px;">${userLocation.municipio||userLocation.provincia}</p>`
 }
