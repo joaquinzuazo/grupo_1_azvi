a509e4bda1b986005421ce567817c681969ab2ef
Facundo Aquino <facun90@live.com>
2021-03-31 23:50:47 -0300

    products pagination

--- src/controllers/api/productsController.js
+++ src/controllers/api/productsController.js
@@ -7,15 +7,23 @@ const refactorProduct = require('../helpers/refactorProduct')
 let productsControllers = {
 	allProducts: async (req = request, res = response) => {
 		try {
+			const { page = null } = req.query
+			console.log({ page })
 			const { count, rows: products } = await db.providers.findAndCountAll({
 				include: [{ association: 'categories' }, { association: 'services' }],
+				offset: page * 10,
+				limit: page && 10,
 			})
+			const pages = Math.ceil(count / 10) - 1
 			const categories = await db.categories.findAll({
 				include: [{ association: 'providers' }],
 			})
+
 			const response = {
 				url: req.originalUrl,
 				count,
+				previous: page > 0,
+				next: page != pages,
 				countByCategory: refactorCategorie(categories),
 				products: refactorProduct(products),
 			}
