46653e2e314ef8420800e25c3df33937816c8022
tomi1608 <ibaneztomas1995@gmail.com>
2021-04-07 19:53:13 -0300

    Arregle el usersController

--- src/controllers/api/usersControllers.js
+++ src/controllers/api/usersControllers.js
@@ -10,7 +10,7 @@ let usersControllers = {
 
      const response = {
          count, 
-         users: userDetail,
+         users: usersDetails,
      }                                 
      res.send(response)
  },
