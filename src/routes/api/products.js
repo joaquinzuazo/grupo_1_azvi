82a6c4a3ca14b03ba5cb6e4c10eccfac8f9cd46b
Facundo Aquino <facun90@live.com>
2021-04-12 17:43:21 -0300

    chages on products api

--- src/routes/api/products.js
+++ src/routes/api/products.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var productsAPIController = require('../../controllers/api/productsController')
 
-router.get('/all', productsAPIController.allProducts);
+router.get('/', productsAPIController.allProducts);
 router.get('/detail/:id', productsAPIController.productDetail);
 router.get('/latest', productsAPIController.latest)
 
